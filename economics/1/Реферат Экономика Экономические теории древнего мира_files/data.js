1de1e04c5e9491c959b69923336df67bda5f7b77
Dzyuba Vlad <Dzyubavlad@gmail.com>
2016-09-13 16:23:41 +0300

    Economics older

---
+++ economics/1/Реферат Экономика Экономические теории древнего мира_files/data.js
@@ -0,0 +1,9 @@
+function showtrash(){
+//var menu =  '<a href="/index.html"><img src="/images/b_main.gif" width="133" height="23" border="0"></a><br><a href="/prise.html"><img src="/images/b_prise.jpg" width="133" height="23" border="0"></a><br><a href="/payments.html"><img src="/images/b_pay.gif" width="133" height="23" border="0"></a><br><a href="/shema.html"><img src="/images/b_shema.gif" width="133" height="23" border="0"></a><br><a href="/order/member.php"><img src="/images/b_order.gif" width="133" height="23" border="0"></a><br><a href="/colaboration.html"><img src="/images/b_colaboration.gif" width="133" height="23" border="0"></a><br><a href="/data/index.html"><img src="/images/b_referats.gif" width="133" height="23" border="0"></a><br><a href="/order/contacts.php"><img src="/images/b_contacts.gif" width="133" height="23" border="0"></a><br>';
+//var place = document.getElementById("trash");
+//place.innerHTML = menu;
+var sometrash = '<span style="position:absolute; top:10px; left:340px; width:500px; height: 80px;"></span><span  style="position:absolute; top:110px; left:440px; width:560px; height: 130px;"><iframe id="orphus" src="/orphus/orphus.htm#!shop@referater.ru" width="257" height="48" frameborder="0" scrolling="no"></iframe></span>';
+var bottomp = document.getElementById("bottom");
+bottomp.innerHTML = sometrash;
+}
+
