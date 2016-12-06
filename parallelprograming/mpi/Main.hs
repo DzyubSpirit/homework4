54406547129abdc8e8ff259f4c23224c04f71dcb
Dzyuba Vlad <Dzyubavlad@gmail.com>
2016-12-06 19:04:09 +0200

    Added writing all messages

--- parallelprograming/mpi/Main.hs
+++ parallelprograming/mpi/Main.hs
@@ -1,19 +1,20 @@
 module Main where
 
 import Control.Parallel.MPI.Simple (mpiWorld, commWorld, unitTag, send, recv)
+import Control.Monad (forM_, when, replicateM)
 import System.Random (getStdGen, randomRs)
 import Data.List (intercalate, transpose)
 
-size = 500
+size = 2
 
 main :: IO ()
-main = mpiWorld $ \size rank -> do
+main = mpiWorld $ \size rank ->
   case rank of
-    3 -> do (msg, _status) <- recv commWorld 1 unitTag
-            putStrLn msg
-    i -> if i < 3
-         then funcs !! (fromInteger $ toInteger i) >>= send commWorld 3 unitTag
-         else return ()
+    3 -> forM_ [0..2] $ \i -> do
+      (msg, _status) <- recv commWorld i unitTag
+      putStrLn msg
+    i -> when (i < 3) $
+           funcs !! fromInteger (toInteger i) >>= send commWorld 3 unitTag
     {-0 -> do (msg, _status) <- recv commWorld 1 unitTag-}
             {-putStrLn msg-}
     {-1 -> send commWorld 0 unitTag "Hello World"-}
@@ -23,22 +24,22 @@ funcs = [f1, f2, f3]
 
 f1 :: IO String
 f1 = do
-  [b, c, ma, me] <- sequence $ replicate 4 randMatrix
+  [b, c, ma, me] <- replicateM 4 randMatrix
   let res = mult (mult b c) (mult ma me)
   return $ showResults ["b", "c", "ma", "me", "res"]
                        [b, c, ma, me, res]
 
 f2 :: IO String
 f2 = do
-  [mk, mh, mf] <- sequence $ replicate 3 randMatrix
+  [mk, mh, mf] <- replicateM 3 randMatrix
   let res = mult (transpose mk) (mult mh mf)
   return $ showResults ["mk", "mh", "mf", "res"]
                        [mk, mh, mf, res]
 
 f3 :: IO String
 f3 = do
-  [mp, mr, t] <- sequence $ replicate 3 randMatrix
-  let res = mult [[foldr1 max . concat $ mult mp mr]] t
+  [mp, mr, t] <- replicateM 3 randMatrix
+  let res = mult [[maximum . concat $ mult mp mr]] t
   return $ showResults ["mp", "mr", "t", "res"]
                        [mp, mr, t, res]
 
@@ -46,7 +47,7 @@ f3 = do
 
 
 showResults :: [String] -> [[[Int]]] -> String
-showResults names matrixes= foldr (++) "" . map (\(a, b) -> concat [a, ":\n", b, "\n"])
+showResults names matrixes= concatMap (\(a, b) -> concat [a, ":\n", b, "\n"])
     . zip names $ map showMatrix matrixes
 
 showMatrix :: Show a => [[a]] -> String
@@ -61,4 +62,4 @@ randMatrix :: IO [[Int]]
 randMatrix = splitArray size . randomRs (-10, 10) <$> getStdGen
                            
 splitArray :: Int -> [a] -> [[a]]
-splitArray n = take n . map fst . tail . iterate (splitAt n . snd) . ((,) [])
+splitArray n = take n . map fst . tail . iterate (splitAt n . snd) . (,) []
